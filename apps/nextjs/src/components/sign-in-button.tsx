6d160030a601da21343a140a819244e244c6df83
aikhe <ikeandrie.ro@gmail.com>
2026-05-07 06:01:04 +0800

    feat(nextjs): update login layout & sign-in button

--- apps/nextjs/src/components/sign-in-button.tsx
+++ apps/nextjs/src/components/sign-in-button.tsx
@@ -9,24 +9,37 @@ interface SignInButtonProps {
   children: ReactNode;
   className?: string;
   callbackURL?: string;
+  provider?: "discord" | "google";
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  size?: "default" | "sm" | "lg" | "icon";
 }
 
 export function SignInButton({
   children,
   className,
   callbackURL = "/",
+  provider = "discord",
+  variant,
+  size = "lg",
 }: SignInButtonProps) {
   return (
     <form>
       <Button
-        size="lg"
+        size={size}
+        variant={variant}
         className={className}
         formAction={async () => {
           "use server";
 
           const result = await auth.api.signInSocial({
             body: {
-              provider: "discord",
+              provider,
               callbackURL,
             },
           });
