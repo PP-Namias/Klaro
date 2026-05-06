88af234564cfab8777b346f3c5e2072440783d4a
aikhe <ikeandrie.ro@gmail.com>
2026-05-07 06:00:43 +0800

    feat(nextjs): refresh landing page layout

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,1 +1,30 @@
-export { default } from "./sample-landing/page";
+import { Clarity } from "~/layouts/Landing/Clarity";
+import { CTA } from "~/layouts/Landing/CTA";
+import { Features } from "~/layouts/Landing/Features";
+import { Footer } from "~/layouts/Landing/Footer";
+import { Hero } from "~/layouts/Landing/Hero";
+import { MoreThanScanning } from "~/layouts/Landing/MoreThanScanning";
+import { Navbar } from "~/layouts/Landing/Navbar";
+import { Security } from "~/layouts/Landing/Security";
+import { Testimonials } from "~/layouts/Landing/Testimonials";
+import styles from "./page.module.css";
+
+export default function HomePage() {
+	return (
+		<div className={`${styles.page} overflow-x-hidden`}>
+			<Navbar />
+
+			<Hero />
+			<main className={styles.main}>
+				<Clarity />
+				<Features />
+				<MoreThanScanning />
+				<Security />
+				<Testimonials />
+				<CTA />
+			</main>
+
+			<Footer />
+		</div>
+	);
+}
