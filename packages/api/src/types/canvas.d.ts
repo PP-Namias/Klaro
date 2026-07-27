2b466141ce5b76510db3a22f775d2dfc78231a4f
Jhon Keneth Namias <jkrbn99@gmail.com>
2026-07-27 15:04:16 +0800

    style: apply prettier formatting to canvas.d.ts

--- packages/api/src/types/canvas.d.ts
+++ packages/api/src/types/canvas.d.ts
@@ -1,7 +1,4 @@
 declare module "canvas" {
-  export function createCanvas(
-    width: number,
-    height: number,
-  ): any;
+  export function createCanvas(width: number, height: number): any;
   export function loadImage(source: Buffer): Promise<any>;
 }
